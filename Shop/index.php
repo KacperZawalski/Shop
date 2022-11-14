a0294c58d22d10e431a0c59e679f16cfca35fb4d
KacperZawalski <110179912+KacperZawalski@users.noreply.github.com>
2022-11-15 00:08:48 +0100

    Add files via upload

---
+++ Shop/index.php
@@ -0,0 +1,32 @@
+<html>
+	<title>
+	</title>
+	<head>
+		<link rel="stylesheet" href="main.css">
+	</head>
+	<body class="body">
+		<div class="container">
+			<div class="navBar">
+				<div id="shopLogo" class="navItems">Logo</div>
+				<div class="search">
+					<form class="searchForm">
+							<input type="text" class="searchBar"></input>
+							<input type="image" class="searchButton" src="lupa.jpg"></input>
+					</form>
+				</div>
+				<ol class="navItems">Category
+					<ul class="categoryList">
+						<li class="categoryListItem">1</li>
+						<li class="categoryListItem">2</li>
+						<li class="categoryListItem">3</li>
+						<li class="categoryListItem">4</li>
+					</ul>
+				</ol>
+				<div id="basket" class="navItems">Basket</div>
+				<div id="account" class="navItems">Account</div>
+			</div>
+			<div class="main"></div>
+			<div class="footer"></div>
+		</div>
+	</body>
+</html>
